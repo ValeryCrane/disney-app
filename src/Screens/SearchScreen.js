1ee73315830627b7653bdb5a87f784713d95372a
Valeriy Zhuravlev <71835639+ValeryCrane@users.noreply.github.com>
2023-03-15 23:00:57 +0300

    Added random character functionality

--- src/Screens/SearchScreen.js
+++ src/Screens/SearchScreen.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View, TouchableOpacity, ActivityIndicator } from "react-native";
-import { Entypo, AntDesign } from '@expo/vector-icons';
+import { Entypo, AntDesign, FontAwesome5 } from '@expo/vector-icons';
 import SearchHeader from "../Elements/SearchHeader";
 import ListOfCharacters from "../Elements/ListOfCharacters";
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 // URL для запросов поиска персонажей.
 const ALL_CHARACTERS_URL = 'https://api.disneyapi.dev/characters'
 const SEARCH_URL = 'https://api.disneyapi.dev/character?name='
+const CHARACTER_URL = 'https://api.disneyapi.dev/characters/'
 
 // Экран с возможностью поиска персонажей.
 export default function SearchScreen({ navigation }) {
+
+    const safeArea = useSafeAreaInsets()
+
     const [characters, setCharacters] = useState([])
     const [isLoading, setIsLoading] = useState(false)
 
@@ -32,6 +37,19 @@ export default function SearchScreen({ navigation }) {
                     }
                 }))
                 setIsLoading(false)
+            })
+    }
+
+    const goToRandomCharacter = () => {
+        let url = CHARACTER_URL + Math.floor(Math.random() * 7397 + 130)
+        console.log(url)
+        fetch(url)
+            .then((response) => response.json())
+            .then((json) => {
+                navigation.navigate("Character", {
+                    ...json,
+                    key: json._id
+                })
             })
     }
 
@@ -58,42 +76,48 @@ export default function SearchScreen({ navigation }) {
 
             }
             
-            <TouchableOpacity style={styles.cloudButton} activeOpacity={1} onPress={() => {
-                navigation.navigate('CloudSettings')
-            }}>
-                <AntDesign name="cloud" size={24} color="white" />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.listButton} activeOpacity={1} onPress={() => {
-                navigation.navigate('Groups')
-            }}>
-                <Entypo name="list" size={24} color="white" />
-            </TouchableOpacity>
+            <View style={[styles.bottomRightCorner, {bottom: 16 + safeArea.bottom}]}>
+                <TouchableOpacity style={styles.cornerButton} activeOpacity={1} onPress={() => {
+                    navigation.navigate('Groups')
+                }}>
+                    <Entypo name="list" size={24} color="white" />
+                </TouchableOpacity>
+            </View>
+            <View style={[styles.bottomLeftCorner, {bottom: 16 + safeArea.bottom}]}>
+                <TouchableOpacity style={styles.cornerButton} activeOpacity={1} onPress={() => {
+                    navigation.navigate('CloudSettings')
+                }}>
+                    <AntDesign name="cloud" size={24} color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.cornerButton} activeOpacity={1} onPress={() => {
+                    goToRandomCharacter()
+                }}>
+                    <FontAwesome5 name="dice" size={24} color="white" />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    listButton: {
-        position: 'absolute',
-        bottom: 32,
-        right: 32,
+    cornerButton: {
         width: 56,
         height: 56,
+        margin: 4,
         borderRadius: 28,
         backgroundColor: '#ff7a59',
         justifyContent: 'center',
         alignItems: 'center'
     },
-    cloudButton: {
+    bottomLeftCorner: {
         position: 'absolute',
-        bottom: 32,
-        left: 32,
-        width: 56,
-        height: 56,
-        borderRadius: 28,
-        backgroundColor: '#ff7a59',
-        justifyContent: 'center',
-        alignItems: 'center'
+        bottom: 16,
+        left: 16,
+    },
+    bottomRightCorner: {
+        position: 'absolute',
+        bottom: 16,
+        right: 16,
     },
     activityIndicator: {
         flex: 1
